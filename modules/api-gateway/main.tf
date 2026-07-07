3747b876657f9d29a6b5811b952fd2920500adce
jivan-rathod <ratjivan2026@gmail.com>
2026-07-07 20:28:52 +0530

    added initialization files 2

---
+++ modules/api-gateway/main.tf
@@ -0,0 +1,82 @@
+resource "aws_api_gateway_rest_api" "api" {
+
+  name = "${var.project_name}-${var.environment}-api"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+
+}
+
+resource "aws_api_gateway_resource" "report" {
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  parent_id = aws_api_gateway_rest_api.api.root_resource_id
+
+  path_part = "report"
+
+}
+
+resource "aws_api_gateway_method" "post" {
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  resource_id = aws_api_gateway_resource.report.id
+
+  http_method = "POST"
+
+  authorization = "NONE"
+
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  resource_id = aws_api_gateway_resource.report.id
+
+  http_method = aws_api_gateway_method.post.http_method
+
+  integration_http_method = "POST"
+
+  type = "AWS_PROXY"
+
+  uri = var.lambda_invoke_arn
+
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+
+  depends_on = [
+    aws_api_gateway_integration.lambda
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+}
+
+resource "aws_api_gateway_stage" "dev" {
+
+  stage_name = var.environment
+
+  deployment_id = aws_api_gateway_deployment.deployment.id
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+
+  statement_id = "AllowExecutionFromAPIGateway"
+
+  action = "lambda:InvokeFunction"
+
+  function_name = var.lambda_function_name
+
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+
+}
+
